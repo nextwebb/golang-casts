027b6dcb75458d0711c744997745d3b330f40cfa
nextwebb <oaikhenahpeterson@gmail.com>
2020-12-17 01:36:03 +0100

    creating a new deck

--- deck.go
+++ deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck [] string // we can think about this as the class declaration
 
+// like static methods in js
+func newDeck() deck {
+	// var cards []string
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardsValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardsValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // go receiver
 func (d deck) print() {
 	for i, card := range d {
