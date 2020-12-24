85f03ebc438d283b53d8e7d96407fc48f0820352
nextwebb <oaikhenahpeterson@gmail.com>
2020-12-24 22:16:59 +0100

    returning multiple results

--- deck.go
+++ deck.go
@@ -9,7 +9,7 @@ type deck [] string // we can think about this as the class declaration
 // like static methods in js
 func newDeck() deck {
 	// var cards []string
-	cards := deck{}
+	cards := deck{} // choose data type
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardsValues := []string{"Ace", "Two", "Three", "Four"}
@@ -22,11 +22,19 @@ func newDeck() deck {
 
 	return cards
 }
+// slices are zero indexed
 
 // go receiver
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	// expecting two separate values
+	// [start - 5] // [5 - end]
+ 	return d[:handSize], d[handSize:]
+
 }
 
--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 func main()  {
 	cards := newDeck()
-	cards.print()
+	// deck datatype , calling the go receiver
+	hand, remainingCards := deal(cards, 5)
+	// the two variables returns a datatype of deck, so it has access to the go receivers belonging to type "deck"
+	hand.print()
+	remainingCards.print()
 }
 
 
